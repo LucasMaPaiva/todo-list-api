6e3a4fa4522c8677f3b8854690f27aadee3ef7ae
root <root@DEV-09>
2024-09-11 09:04:08 -0400

    finishin endpoint create task situation

--- app/Services/CreateTaskSituationService.php
+++ app/Services/CreateTaskSituationService.php
@@ -5,27 +5,41 @@ namespace App\Services;
 use App\Models\TaskSituation;
 use App\Services\Contracts\CreateTaskSituationServiceContract;
 use App\DataTransferObjects\CreateTaskSituationDTO;
+use App\Repository\Contracts\TaskSituationRepositoryContract;
+use Exception;
 
 class CreateTaskSituationService implements Contracts\CreateTaskSituationServiceContract
 {
+
+    private TaskSituationRepositoryContract $taskSituationRepository;
+
+    public function __construct()
+    {
+        $this->repository();
+    }
+
+    public function repository() :void
+    {
+        $this->taskSituationRepository = app(TaskSituationRepositoryContract::class);
+    }
+
     /**
      * @param $request
      * @return TaskSituation
+     * @throws Exception
      */
      public function execute($request): TaskSituation
      {
-        $itemName = $request->input('name');
-
-
-        if (empty($itemName)) {
-            throw new \InvalidArgumentException('O campo name não pode ser vazio.');
-        }
-
-        $taskSituation = new TaskSituation();
-        $taskSituation->name = $itemName;
-
-        $taskSituation->save();
-
-        return $taskSituation;
+         try {
+             return $this->taskSituationRepository->create([
+                 'name' => $request->name
+             ]);
+         } catch (Exception $exception) {
+             $this->logException(
+                 caller: __METHOD__,
+                 exception: $exception,
+             );
+             throw $exception;
+         }
     }
 }
