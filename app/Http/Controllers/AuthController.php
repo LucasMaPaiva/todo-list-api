8ff7f8fd271f6e0121d1837d8b9c96f56153f682
paiva <lucas.paiva@ae3tecnologia.com.br>
2024-09-14 21:13:25 -0400

    add function register user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,19 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\DataTransferObjects\Auth\LoginDTO;
+use App\DataTransferObjects\Auth\RegisterUserDTO;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterUserRequest;
 use App\Http\Resources\Auth\LoginResource;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 use InvalidArgumentException;
 use Illuminate\Routing\Controller;
 use App\Services\Auth\Contracts\LoginServiceContract;
+use App\Services\Auth\Contracts\RegisterUserServiceContract;
 
 
 class AuthController extends Controller
 {
 
     private LoginServiceContract $loginService;
+
+    private RegisterUserServiceContract $registerUserService;
 
     public function __construct()
     {
@@ -25,6 +31,7 @@ class AuthController extends Controller
     public function services() :void
     {
         $this->loginService = app(LoginServiceContract::class);
+        $this->registerUserService = app(RegisterUserServiceContract::class);
     }
 
     /**
@@ -43,6 +50,33 @@ class AuthController extends Controller
                 message: __('messages.login.success'),
                 status_code: 201
             );
+        } catch (ModelNotFoundException $modelNotFoundException) {
+            return self::modelNotFoundResponse($modelNotFoundException);
+        } catch (InvalidArgumentException $invalidArgumentException) {
+            return self::invalidArgumentResponse($invalidArgumentException);
+        } catch (Exception $exception) {
+            return self::internalServerErrorResponse($exception);
+        }
+    }
+
+    /**
+     * @param RegisterUserRequest $request
+     * @return JsonResponse
+     */
+    public function register(RegisterUserRequest $request) :JsonResponse
+    {
+        try {
+
+            return self::successResponse(
+                data: LoginResource::make(
+                    $this->registerUserService->execute(
+                        RegisterUserDTO::fromRequest($request)
+                    ),
+                ),
+                message: __('messages.user.created'),
+                status_code: 201
+            );
+
         } catch (ModelNotFoundException $modelNotFoundException) {
             return self::modelNotFoundResponse($modelNotFoundException);
         } catch (InvalidArgumentException $invalidArgumentException) {
