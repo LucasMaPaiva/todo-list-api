2ce51578926628ae0beb2ae33d439dc4a13165f4
paiva <lucas.paiva@ae3tecnologia.com.br>
2024-09-15 22:39:40 -0400

    add dto, resource and request tag

---
+++ app/Http/Resources/Tag/CreateTagResource.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Resources\Tag;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CreateTagResource extends JsonResource {
+    public function toArray(Request $request): array {
+        return [
+            'name' => $this->name,
+        ];
+    }
+}
